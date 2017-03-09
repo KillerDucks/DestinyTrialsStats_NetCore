a4eeea0b26aa588ecd03d60f0b663e831da399cb
KillerDucks <n.patel.coventry@gmail.com>
2017-03-09 08:12:02 +0000

    Add project files.

---
+++ DestinyTrialsStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DestinyTrialsStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
