9d0ee2d36856ac503c9f6325e331ef031eb80a1a
KillerDucks <n.patel.coventry@gmail.com>
2017-03-10 12:07:58 +0000

    Added Main functions

--- DestinyTrialsStats/Program.cs
+++ DestinyTrialsStats/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Net;
 using System.Net.Http;
@@ -21,7 +20,17 @@ namespace DestinyTrialsStats
                 {
                     Console.WriteLine("Success!");
                     Console.WriteLine("We are now processing your request");
-                    GetTrialsData();
+                    //GetTrialsData();
+                    if (1 == 1/*args[1] == "GetBungieAcc"*/)
+                    {
+                        if (GetBungieAcc("TacticalNexus4") == "SUCCESS")
+                        {
+                            if (GetDestinyAccount() == "SUCCESS")
+                            {
+                                GetTrialsData();
+                            }
+                        }
+                    }
                 }
             }
             Console.ReadKey();
@@ -78,19 +87,66 @@ namespace DestinyTrialsStats
             {
                 using (var client = new HttpClient())
                 {
+                    int x = 0;
+                    foreach (var i in PlayerCharater.Characters)
+                    {
+                        client.DefaultRequestHeaders.Add("X-API-Key", "1d7e2d7bb0db43618efa3993660282ae");
+                        var res = client.GetAsync("https://www.bungie.net/platform/Destiny/Stats/1/" + PlayerCharater.DestMembID + "/" + PlayerCharater.Characters[x++] + "/?modes=TrialsOfOsiris").Result;
+                        if (res.IsSuccessStatusCode)
+                        {
+                            string resultContent = res.Content.ReadAsStringAsync().Result;
+                            JObject json = JObject.Parse(resultContent);
+                            dynamic jsonDe = JsonConvert.DeserializeObject(json.ToString());
+                            Console.WriteLine(jsonDe.Response.trialsOfOsiris.allTime.killsDeathsRatio.basic.displayValue);
+                            return res.ToString();
+                        }
+                        else
+                        {
+                            Console.WriteLine("TrialsData Error");
+                            return "ERROR";
+                        }
+                    }
+                    return "NOT HERE TRIALS";
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static string GetBungieAcc(string GamerTag)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
                     client.DefaultRequestHeaders.Add("X-API-Key", "1d7e2d7bb0db43618efa3993660282ae");
-                    var res = client.GetAsync("http://www.bungie.net/platform/Destiny/Stats/1/4611686018452351232/2305843009326756237/?modes=TrialsOfOsiris").Result;
+                    var res = client.GetAsync("https://www.bungie.net/Platform/User/SearchUsers/?q=" + GamerTag).Result;
                     if (res.IsSuccessStatusCode)
                     {
                         string resultContent = res.Content.ReadAsStringAsync().Result;
                         JObject json = JObject.Parse(resultContent);
                         dynamic jsonDe = JsonConvert.DeserializeObject(json.ToString());
-                        Console.WriteLine(jsonDe.Response.trialsOfOsiris.allTime.killsDeathsRatio.basic.displayValue);
-                        return res.ToString();
+                        foreach(var i in jsonDe)
+                        {
+                            if (jsonDe.Response[0].xboxDisplayName == GamerTag)
+                            {
+                                PlayerCharater.BungMembID = jsonDe.Response[0].membershipId;
+                                Console.WriteLine(jsonDe.Response[0].membershipId);
+                                return "SUCCESS";
+                            }
+                            else
+                            {
+                                return "NOID";
+                            }
+                        }
+
+                        return "Error in ForEach In";
                     }
                     else
                     {
-                        return "ERROR";
+                        return "";
                     }
                 }
             }
@@ -100,27 +156,45 @@ namespace DestinyTrialsStats
             }
         }
 
-        public static string GetBungieAcc()
+        public static string GetDestinyAccount()
         {
             try
             {
                 using (var client = new HttpClient())
                 {
-                    var res = client.GetAsync("http://www.bungie.net/").Result;
+                    client.DefaultRequestHeaders.Add("X-API-Key", "1d7e2d7bb0db43618efa3993660282ae");
+                    var res = client.GetAsync("https://www.bungie.net/platform/User/GetBungieAccount/" + PlayerCharater.BungMembID + "/254/").Result;
                     if (res.IsSuccessStatusCode)
                     {
-                        return "";
+                        string resultContent = res.Content.ReadAsStringAsync().Result;
+                        JObject json = JObject.Parse(resultContent);
+                        dynamic jsonDe = JsonConvert.DeserializeObject(json.ToString());
+                        foreach (var i in jsonDe)
+                        {
+                            PlayerCharater.Characters[i] = jsonDe.Response.destinyAccounts[0].characters[i].characterId;
+                            PlayerCharater.CharactersInfo[i] = jsonDe.Response.destinyAccounts[0].characters[i].characterClass.className;
+                        }
+                        PlayerCharater.DestMembID = jsonDe.Response.destinyAccounts[0].userInfo.membershipId;
+                        return "SUCCESS";
                     }
                     else
                     {
-                        return "";
+                        return "HTTP ERROR";
                     }
                 }
             }
             catch
             {
-                return "";
+                return "Unexpected Exception";
             }
         }
     }
+
+    public static class PlayerCharater
+    {
+        public static string BungMembID { get; set; }
+        public static string DestMembID { get; set; }
+        public static String[] Characters = new string[4];
+        public static String[] CharactersInfo = new string[4];
+    }   
 }
